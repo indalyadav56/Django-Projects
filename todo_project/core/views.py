76de6dab1f9ac4807a0c2a8b13407c055cc19162
indalkumaryadav <yadavindal269@gmail.com>
2021-04-10 12:09:06 +0530

    first commit

--- todo_project/core/views.py
+++ todo_project/core/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.views.generic import View
+from .models import Todo
+from .forms import TodoCreateForm
 # Create your views here.
+
+class HomeView(View):
+    
+    def get(self,request):
+        todo=Todo.objects.all()
+        todo_form=TodoCreateForm()
+
+        context={
+            'todo':todo,
+            'form':todo_form
+        }
+        return render(request,'core/home.html',context)
+    
+    def post(self,request):
+        todo_form=TodoCreateForm(request.POST)
+        if todo_form.is_valid():
+            todo_form.save()
+            return redirect("/")
+        context={
+            'form':todo_form
+        }
+        return render(request,'core/home.html',context)
+    
+    def delete(self,request,pk):
+        todo=Todo.objects.get(id=pk)
+        todo.delete()
+        return render(request,"core/delete.html")
--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'indal',                      
+        'USER': 'postgres',
+        'PASSWORD': 'indal',
+        'HOST': 'localhost',
     }
 }
 
@@ -118,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    BASE_DIR / 'static'
+]
