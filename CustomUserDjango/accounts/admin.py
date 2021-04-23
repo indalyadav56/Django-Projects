771f00bcb5e76fc6d204c5f2f136513f81e4164f
Indal Kumar <73450527+indalkumaryadav@users.noreply.github.com>
2021-04-23 17:45:01 +0530

    Update admin.py

--- CustomUserDjango/accounts/admin.py
+++ CustomUserDjango/accounts/admin.py
@@ -1,28 +1,27 @@
 from django.contrib import admin
-from django.db.models.fields import TextField
-from .models import NewUser
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth import get_user_model
 
 
-class UserAdminConfig(UserAdmin):
-    ordering = ('-start_date',)
-    list_display = ('email', 'is_active', 'is_staff')
-    search_fields = ('eamil',)
-    list_filter = ('email', 'is_active')
-
+class CustomUserAdmin(UserAdmin):
+    """Define admin model for custom User model with no username field."""
     fieldsets = (
-        (None, {'fields': ('email',)}),
-        ('Permissions', {'fields': ('is_staff', 'is_active')}),
-        ('Personal', {'fields': ('about',)}),
-
+        (None, {'fields': ('email', 'password')}),
+        # (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
-
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_active', 'is_staff'),
+            'fields': ('email', 'password1', 'password2'),
         }),
     )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
 
 
-admin.site.register(NewUser, UserAdminConfig)
+admin.site.register(get_user_model(), CustomUserAdmin)
